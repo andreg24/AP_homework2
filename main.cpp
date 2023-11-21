c65444f92e827c986ff7c5f36086fd86f786d253
3sara <84321374+3sara@users.noreply.github.com>
2023-11-21 18:22:04 +0100

    con iteratore main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,25 @@
 #include "csvparser.hpp"
 
 int main() {
-	CSVParser A("csv_examples/top_english_movies100.csv");
 
-	//CSVParser A("csv_examples/prove.csv");
+
+	CSVParser A("top_english_movies.csv");
 	A.read();
-	vector<variant<vector<optional<string>>, vector<optional<double>>>> x = A.getDataset();
-	/*
-	cout << x.size() << endl;;
-	vector<optional<double>> y = get<vector<optional<double>>>(x[1]);
-	cout << y[4].value() << endl;
-	cout << "-----" << endl;
-	variant<optional<string>, optional<double>> a = A(4,1);
-  optional<double> b = get<optional<double>>(a);
-	cout << b.value() << endl;
-	*/
+	
+	std::cout<< "La media è: "<<A.mean_col(0)<< ". La varianza è: "<< A.var_col(0)<< ". La mediana è: "<< A.median_col(0)<< ". La deviazione standard è: "<< A.dev_std(0)<<std::endl;
+	
+	  for (const auto& col: A){
+		std::cout<<"cambio colonna"<<std::endl;
+		if (holds_alternative<vector<optional<string>>>(col)) {
+           const auto& str_vec = get<vector<optional<string>>>(col);
+           if (!str_vec.empty()) {
+               cout << *str_vec[0] << endl;
+           }
+       } else if (holds_alternative<vector<optional<double>>>(col)) {
+           const auto& double_vec = get<vector<optional<double>>>(col);
+           if (!double_vec.empty()) {
+               cout << *double_vec[0] << endl;
+           }
+  }}
+	
 }
