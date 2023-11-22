48e6c0230bf769a7801a320540ce7385cb7cb42a
davidededominicis <83349571+davidededominicis@users.noreply.github.com>
2023-11-22 11:40:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 int main() {
 
 
-	CSVParser A("top_english_movies.csv");
+	CSVParser A("top_english_movies2.csv");
 	A.read();
 	
-	std::cout<< "La media è: "<<A.mean_col(0)<< ". La varianza è: "<< A.var_col(0)<< ". La mediana è: "<< A.median_col(0)<< ". La deviazione standard è: "<< A.dev_std(0)<<std::endl;
+	std::cout<< "La media è: "<<A.mean_col(0)<< ". La varianza è: "<< A.var_col(0)<< ". La mediana è: "<< A.median_col(0)<< ". La deviazione standard è: "<< A.dev_std(0)<< ". La correlazione è: "<< A.correlation_analysis(0,2)<<std::endl;
 	
 	  for (const auto& col: A){
 		std::cout<<"cambio colonna"<<std::endl;
@@ -20,6 +20,12 @@ int main() {
            if (!double_vec.empty()) {
                cout << *double_vec[0] << endl;
            }
+
   }}
+
+  map<string, int> freq = A.countFrequency(2);
+  for (const auto& pair : freq ) {
+    cout<< "Elemento: " << pair.first << " Frequenza: " << pair.second<< std::endl;
+  }
 	
 }
