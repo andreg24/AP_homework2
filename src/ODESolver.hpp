9cb919e44f67619dfe3cd0a1c226ae7e9385419d
davidededominicis <83349571+davidededominicis@users.noreply.github.com>
2023-11-30 15:11:09 +0100

    valutations ODESolver.hpp

--- src/ODESolver.hpp
+++ src/ODESolver.hpp
@@ -21,43 +21,67 @@ public:
     ODESolver(function<VectorXd(double, VectorXd)> fun, double t_start, double t_end, VectorXd y0);
 
     //Runge Kutta 4 method for solving ODEs
-    vector<VectorXd> RK4(unsigned int n);
+    pair<VectorXd, vector<VectorXd>> RK4(unsigned int n);
 
     //Variation of RK4 that saves the results into a csv
     void RK4_csv(unsigned int n);
 
     //Midpoint method for solving ODEs
-    vector<VectorXd> midpoint(unsigned int n);
+    pair<VectorXd, vector<VectorXd>> midpoint(unsigned int n);
 
     //Variation of midpoint method that saves the results into a csv
     void midpoint_csv(unsigned int n);
 
     // Forward Euler methord for solving ODEs
-    vector<VectorXd> euler(unsigned int n);
+    pair<VectorXd, vector<VectorXd>> euler(unsigned int n);
 
     // Variation of euler method that saves the results into a csv
     void euler_csv(unsigned int n);
 
     // Accuracy
-    double accuracy(vector<VectorXd> Y, function<VectorXd(double)> analitic) {
-      double max_error = 0.0;
-      VectorXd diff(Y.size()-1); 
-      cout<<Y[0].size()<<endl;
-      cout<<Y.size()<<endl;
-        for (unsigned int i=0; i < Y[0].size(); i++) //ciclo sulle righe 
-        {
-          for (unsigned int j=1; j<Y.size(); j++) // ciclo sulle colonne a partire dalla seconda 
-          {
-              diff[j-1] = Y[j][i] - analitic(Y[0][i])[j];
-              cout << diff[j-1]<<endl;
-          }
-          max_error = max(diff.norm(), max_error);
-          
+    double accuracy(pair<VectorXd, vector<VectorXd>>& res, function<VectorXd(double)> analitic);
+
+    //Convergence
+    unsigned int convergence(pair<VectorXd, vector<VectorXd>>& res, function<VectorXd(double)> analitic);
+
+    //Efficiency
+    void testEfficiency(string x, unsigned int n);
+
+    //Stability
+    void testStability(string x, pair<VectorXd, vector<VectorXd>>& res);
+
+
+    pair<double, double> convergence2(pair<VectorXd, vector<VectorXd>>& res, function<VectorXd(double)> analytic_solution) {
+        unsigned int n = res.first.size();
+        double h = (t_end - t_start)/n;
+        VectorXd h_values(4); // Valori di h per la discretizzazione
+        h_values << h, h/2, h/4, h/8;
+
+        VectorXd errors(4); // Contiene gli errori massimi per ogni h
+        for (int i = 0; i < h_values.size(); ++i) {
+            auto numerical_solution = euler((t_end - t_start) / h_values(i));
+
+            double max_error = accuracy(numerical_solution, analytic_solution); // Calcola l'errore massimo
+            errors(i) = max_error;
         }
-        return max_error;
+
+        // Calcola l'ordine di convergenza
+        VectorXd convergence_order(3);
+        for (int i = 1; i < errors.size(); ++i) {
+            double order = log(errors(i - 1) / errors(i)) / log(2.0); // Formula per l'ordine di convergenza
+            convergence_order(i - 1) = order;
+
+        }
+        /*for (auto elem : convergence_order) {
+          cout<<" ciao "<< elem << " ciao "<<endl;
+        }*/
+ 
+        // Calcola l'ordine di convergenza medio
+        double avg_order = convergence_order.mean();
+
+        // Restituisce l'errore più piccolo e l'ordine di convergenza medio
+        return make_pair(errors.maxCoeff(), avg_order);
     }
 
-    // Convergence order
-
-
+    
 };
