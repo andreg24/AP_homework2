7db04ab96a38e3a72bdb84d467c393ea5c790777
davidededominicis <83349571+davidededominicis@users.noreply.github.com>
2023-11-30 15:12:16 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -20,6 +20,14 @@ VectorXd fun(double t, VectorXd y) {
    return dy;
 }
 
+VectorXd analitic(double t) {
+   double a = 10;
+   VectorXd y(1);
+   y(0) = exp(-a*t);
+   return y;
+}
+
+
 int main(){
    double t_start=0;
    double t_end=10;
@@ -29,12 +37,12 @@ int main(){
    ODESolver solver= ODESolver( fun, t_start,t_end,y0);
 
    // Testing RK4
-   //std::vector<VectorXd> solution1=solver.RK4(n);
-   /*for (unsigned int j=4990; j<5000; j++){
+   /*std::vector<VectorXd> solution1=solver.RK4(n);
+   for (unsigned int j=0; j<5000; j++){
     std::cout<<solution1[j]<<endl;
        //std::cout << solution.second[j].transpose() << std::endl;
-   }*/
-   //solver.RK4_csv(n);
+   }
+   solver.RK4_csv(n);*/
 
    // Testing midpoint
    //std::vector<VectorXd> solution2=solver.midpoint(n);
@@ -45,12 +53,26 @@ int main(){
    //solver.midpoint_csv(n);
 
    // Testing Euler
-   std::vector<VectorXd> solution3=solver.euler(70);
-   for (unsigned int j=0; j<70; j++){
+   pair<VectorXd, vector<VectorXd>> solution3=solver.euler(70);
+   /*for (unsigned int j=0; j<70; j++){
     std::cout<<solution3[j]<<endl;
        //std::cout << solution.second[j].transpose() << std::endl;
-   }
-   solver.euler_csv(70);
+   }*/
+   solver.euler_csv(70.0);
+
+   //double ghj = solver.accuracy(solution3, analitic);
+   //std::cout << ghj<<endl;
+
+   //solver.testEfficiency("cazzo", 70); 
+   //solver.testStability("euler", solution3);
+
+
+   solver.convergence2(solution3, analitic);
+   std::cout << "The minimal error is: "<< solver.convergence2(solution3, analitic).first<< "The average convergence error is: "<<solver.convergence2(solution3, analitic).second << endl;
+
+
+   
+
 
    return 0;
 }
